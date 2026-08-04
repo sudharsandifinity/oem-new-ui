ace8b2e563838474423ace43fe1b5a17e48bfae2
sudharsan <sudharsan.s@difinitydigital.com>
2026-08-04 16:26:30 +0400

    minnor change

--- src/layout/MainLayout/MenuList/index.jsx
+++ src/layout/MainLayout/MenuList/index.jsx
@@ -34,7 +34,7 @@ const menuIcons = {
   Dashboard: IconDashboard,
   '': IconUsers,
   'Material Request': IconShoppingCart,
-  'Purchase Request': IconFileInvoice,
+  // 'Purchase Request': IconFileInvoice,
   'GRPO': IconBriefcase,
   'Contracting Management': IconBuilding,
   'Approvals': IconClipboardCheck,
